69863e99992e6f562a1a2bc6726ddfd84ce2f832
tad-iizuka <tad.iizuka@gmail.com>
2017-10-13 08:05:36 +0900

    Initial Commit

---
+++ MP3RecordingSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MP3RecordingSample
+//
+//  Created by Tadashi on 2017/10/13.
+//  Copyright © 2017 UBUNIFU Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
